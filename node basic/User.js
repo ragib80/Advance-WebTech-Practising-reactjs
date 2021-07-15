91e0a9c2508f6de1135addb99432da22a113bbc3
ragib80 <ragib.hridoy01@gmail.com>
2021-07-16 03:28:18 +0600

    Node js HTTP Request Handle

---
+++ node basic/User.js
@@ -0,0 +1,12 @@
+export class User {
+
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+    }
+    getName() {
+        return this.name;
+    }
+
+}
+export const role = 'admin';
---
+++ node basic/app.js
@@ -0,0 +1,31 @@
+//const http = require('http');
+//const fs = require('fs');
+import http from 'http';
+import fs from 'fs';
+import { User, role } from './User.js';
+
+const user1 = new User('ragib', '12345');
+
+const server = http.createServer(function (req, res) {
+    console.log('http server working...');
+    console.log(req.url);
+    console.log(req.method);
+    if (req.url == '/home') {
+        res.write(`<h1> hello ${user1.name} from server home ${role} and id is ${user1.id}</h1>`);
+        res.end();
+    }
+
+    if (req.url == '/login') {
+        //res.writeHead(200, { 'content-type': 'text/html' });
+        //res.write('<h5>  hello from login page </h5>');
+        //res.end();
+        fs.createReadStream('login.html').pipe(res);
+    }
+
+    /* res.write('hello from server');
+    res.end();*/
+
+});
+
+server.listen(500);
+console.log("server started at port 500");
